4eb6f6aa5d250ddeaaa00c97f4a7b6229ee82a80
fumiya <fu38.terada@gmail.com>
2019-09-12 15:16:58 +0900

    スマホメニュー、loadingを追加
    sphumburger、loading.scssをファイルを追加

--- html/js/app.js
+++ html/js/app.js
@@ -1,6 +1,28 @@
-$(function () {
-  $(".main-img__bg img:not(:first-child)").hide();
-  setInterval(function () {
-    $(".main-img__bg img:first-child").fadeOut("slow").next("img").fadeIn("slow").end().appendTo(".main-img__bg");
-  }, 3000);
+jQuery(function () {
+  $('.menu').on('click', function () {
+    $('.menu__line').toggleClass('active');
+    $('.gnav').fadeToggle();
+  });
+
+  $(function () {
+    let h = $(window).height(); //ブラウザウィンドウの高さを取得
+    $('.header').css('display', 'none'); //初期状態ではメインコンテンツを非表示
+    $('#loader-bg ,#loader').height(h).css('display', 'block'); //ウィンドウの高さに合わせでローディング画面を表示
+    $(function () {
+      let $allMsg = $('.loader-bg--container__load-text');
+      let $wordList = $('.loader-bg--container__load-text').html().split("");
+      $('.loader-bg--container__load-text').html("");
+      $.each($wordList, function (idx, elem) {
+        let newEL = $("<span/>").text(elem).css({ opacity: 0 });
+        newEL.appendTo($allMsg);
+        newEL.delay(idx * 70);
+        newEL.animate({ opacity: 1 }, 3000);
+      });
+    });
+    $('#loader-bg').delay(3000).fadeOut(900); //または、$('#loader-bg').fadeOut(800);でも可
+    $('#loader').delay(3000).fadeOut(500, function () { }); //または、$('#loader').fadeOut(300);でも可
+    $('.header').css('display', 'block'); // ページ読み込みが終わったらメインコンテンツを表示する
+
+  });
+
 });
